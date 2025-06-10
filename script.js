b9d2638bca71102224108632c15ae8c9aa2b4b73
Raeesah Moola <raeesahmoola529@gmail.com>
2025-06-10 12:48:09 +0200

    Finish task list functionality and styling

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-for(let i = 0; i < 10; i++) {
-    const newItem = document.createElement("input");
-document.getElementById("taskInput").appendChild(newItem);
+const taskList = document.getElementById("taskList");
 
+for(let i = 0; i < 10; i++) {
+    const lineDiv = document.createElement("div");
+    lineDiv.className = "line";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const input = document.createElement("input");
+input.type = "text";
+input.placeholder = `Add a new task.. ${i + 1}`;
+
+    lineDiv.appendChild(checkbox);
+    lineDiv.appendChild(input);
+
+    taskList.appendChild(lineDiv);
 }
 
+ 
